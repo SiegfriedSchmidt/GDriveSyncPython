0018aa7b5f025bd48cc15031ff570c6d3ad385d0
Matvei <matveiyan@yandex.ru>
2024-04-01 20:29:19 +0300

    rearrange imports

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import time
 import logging
 import colorama
 
+from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.http import MediaFileUpload
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaFileUpload
 from pprint import pprint
 from pathlib import Path
 from typing import Tuple
