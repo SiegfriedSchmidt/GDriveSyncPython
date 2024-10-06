5fbc4b7394d4c2d714c6589bcc84677482eb75c6
Matvei <matveiyan@yandex.ru>
2024-10-07 00:12:08 +0300

    Add synology support

--- init.py
+++ init.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
+from typing import Literal
 
+drive_type: Literal['GOOGLE', 'SYNOLOGY'] = os.environ.get("DRIVE_TYPE", "GOOGLE")
 local_folder_path = Path(os.environ.get("LOCAL_FOLDER_PATH", "./folder_sync"))
 auth_key_path = Path(os.environ.get("AUTH_KEY_PATH", "./auth/key.json"))
 remote_folder_name = os.environ.get("REMOTE_FOLDER_NAME", "from_server")
+clear_downloads = int(os.environ.get("CLEAR_DOWNLOADS", "1"))
--- main.py
+++ main.py
@@ -1,16 +1,67 @@
 import os
 import time
+import json
 
 from google.oauth2 import service_account
 from googleapiclient.http import MediaFileUpload
 from googleapiclient.discovery import build
 from pathlib import Path
 
-from init import remote_folder_name, local_folder_path, auth_key_path
+from typing import Iterable, Generator, Tuple
+from abc import ABC, abstractmethod
+from synology_api import filestation
+
+from init import remote_folder_name, local_folder_path, auth_key_path, drive_type, clear_downloads
 from libs.logger import logger
 
 
-class GoogleApi:
+class Drive(ABC):
+    @abstractmethod
+    def show_folder_content(self, folder: str) -> set[str]:
+        pass
+
+    @abstractmethod
+    def upload_files(self, files: Iterable[Tuple[Path, str, str]]) -> Generator[Tuple[str, str], None, None]:
+        # Generator[YieldType, SendType, ReturnType] --- Don't know about SendType :)
+        pass
+
+
+class SynologyFileStation:
+    def __init__(self, ip, port, username, password):
+        self.ip = ip
+        self.port = port
+        self.username = username
+        self.password = password
+        self.file_station: filestation.FileStation
+
+    def __enter__(self):
+        self.file_station = filestation.FileStation(self.ip, self.port, self.username, self.password, secure=True,
+                                                    cert_verify=True, dsm_version=7, debug=False, otp_code=None)
+        return self.file_station
+
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        self.file_station.logout()
+
+
+class SynologyDrive(Drive):
+    def __init__(self, config_path):
+        with open(config_path) as f:
+            config = json.load(f)
+
+        self.file_station = SynologyFileStation(**config)
+
+    def show_folder_content(self, folder: str) -> set[str]:
+        with self.file_station as fs:
+            return set(map(lambda a: a.get('name'), fs.get_file_list(folder)['data']['files']))
+
+    def upload_files(self, files: Iterable[Tuple[Path, str, str]]) -> Generator[Tuple[str, str], None, None]:
+        with self.file_station as fs:
+            for file_path, name, folder in files:
+                rs = fs.upload_file(folder, str(file_path), progress_bar=False, verify=True)
+                yield rs['data']['file'], file_path
+
+
+class GoogleDrive(Drive):
     SCOPES = ['https://www.googleapis.com/auth/drive']
 
     def __init__(self, key_path):
@@ -19,7 +70,7 @@ class GoogleApi:
     @staticmethod
     def get_cred(service_account_json_key):
         return service_account.Credentials.from_service_account_file(filename=service_account_json_key,
-                                                                     scopes=GoogleApi.SCOPES)
+                                                                     scopes=GoogleDrive.SCOPES)
 
     def find_files_by_query(self, query: str):
         # https://developers.google.com/drive/api/guides/ref-search-terms
@@ -46,10 +97,20 @@ class GoogleApi:
     def find_file_id_by_name(self, name: str):
         return list(map(lambda a: a.get('id'), self.find_files_by_query(f"name='{name}'")))
 
-    def show_folder_content(self, folder_id: str):
-        return self.find_files_by_query(f"'{folder_id}' in parents")
+    def get_folder(self, folder_name):
+        folders = self.find_file_id_by_name(folder_name)
+        if len(folders) == 0:
+            raise MyError(f'Remote folder "{folder_name}" not found!')
 
-    def upload_files(self, files: list):
+        if len(folders) > 1:
+            raise MyError(f'Several files were found with the same name "{folder_name}"!')
+
+        return folders[0]
+
+    def show_folder_content(self, folder: str) -> set[str]:
+        return set(map(lambda a: a.get('name'), self.find_files_by_query(f"'{folder}' in parents")))
+
+    def upload_files(self, files: Iterable[Tuple[Path, str, str]]) -> Generator[Tuple[str, str], None, None]:
         with build("drive", "v3", credentials=self.creds) as service:
             for file_path, name, folder_id in files:
                 file_metadata = {
@@ -58,7 +119,7 @@ class GoogleApi:
                 }
                 media = MediaFileUpload(file_path, resumable=True)
                 r = service.files().create(body=file_metadata, media_body=media, fields='id, name').execute()
-                yield r.get('id'), r.get('name')
+                yield r.get('name'), file_path
 
         return
 
@@ -86,17 +147,19 @@ class MyError(BaseException):
     pass
 
 
-def on_find(sync_folder: Path, folder_id: str, gapi: GoogleApi):
+def on_find(sync_folder: Path, remote_folder: str, drive_api: Drive):
     def wrapped(new_files: set[str]):
         logger.info(f'Find new files: {new_files}')
-        folder_content = set(map(lambda a: a.get('name'), gapi.show_folder_content(folder_id)))
+        folder_content = drive_api.show_folder_content(remote_folder)
         diff = new_files - folder_content
         if len(diff) > 0:
             logger.info(f'Files not on the disk: {diff}')
             logger.info(f'Start uploading...')
-            files_to_upload = [(sync_folder / name, name, folder_id) for name in diff]
-            for file_id, file_name in gapi.upload_files(files_to_upload):
-                logger.info(f'"{file_name}" has been uploaded')
+            files_to_upload = [(sync_folder / name, name, remote_folder) for name in diff]
+            for filename, filepath in drive_api.upload_files(files_to_upload):
+                logger.info(f'"{filename}" has been uploaded')
+                if clear_downloads:
+                    os.remove(filepath)
             logger.info(f'Uploading finished')
         else:
             logger.info(f'All new files on disk')
@@ -104,28 +167,27 @@ def on_find(sync_folder: Path, folder_id: str, gapi: GoogleApi):
     return wrapped
 
 
-def get_folder_id(gapi: GoogleApi, folder_name):
-    folders = gapi.find_file_id_by_name(folder_name)
-    if len(folders) == 0:
-        raise MyError(f'Remote folder "{folder_name}" not found!')
-
-    if len(folders) > 1:
-        raise MyError(f'Several files were found with the same name "{folder_name}"!')
-
-    return folders[0]
-
-
 def main():
-    gapi = GoogleApi(auth_key_path)
+    if drive_type == 'GOOGLE':
+        drive_api = GoogleDrive(auth_key_path)
 
-    try:
-        remote_folder_id = get_folder_id(gapi, remote_folder_name)
-    except MyError as error:
-        logger.error(error)
-        exit()
+        try:
+            remote_folder = drive_api.get_folder(remote_folder_name)
+        except MyError as error:
+            return logger.error(error)
+
+        logger.info('Set drive type to GOOGLE')
+
+    elif drive_type == 'SYNOLOGY':
+        drive_api = SynologyDrive(auth_key_path)
+        remote_folder = remote_folder_name
+
+        logger.info('Set drive type to SYNOLOGY')
+    else:
+        return logger.error("Incorrect drive type specified!")
 
     logger.info(f'Remote folder with name "{remote_folder_name}" found')
-    scanner = LocalFolderScanner(local_folder_path, on_find(local_folder_path, remote_folder_id, gapi))
+    scanner = LocalFolderScanner(local_folder_path, on_find(local_folder_path, remote_folder, drive_api))
     logger.info(f'Start scanning')
     scanner.scanning()
 
